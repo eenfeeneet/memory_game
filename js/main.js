d9b60e45372b88e0e0952cfd7dba6b67939a93dc
Hafiz <burnhafiz@gmail.com>
2019-06-19 05:55:04 +0800

    created game logic

--- js/main.js
+++ js/main.js
@@ -23,6 +23,8 @@ cardImage: "images/king-of-diamonds.png"
 ];
 const cardsInPlay = [];
 
+
+
 function checkForMatch() {
 
     if (cardsInPlay[0] == cardsInPlay[1]) {
@@ -32,20 +34,38 @@ function checkForMatch() {
         }
 }
 
-function flipCard(cardId) {
+function flipCard() {
 
-    if (cardsInPlay.length == 2) {
+    var cardId = this.getAttribute('data-id');
 
-        checkForMatch();
-    }
+    this.setAttribute('src', cards[cardId].cardImage);
 
     console.log("User flipped " + cards[cardId].rank);
 
     cardsInPlay.push(cards[cardId].rank);
     console.log(cards[cardId].cardImage);
     console.log(cards[cardId].suit);
+
+    if (cardsInPlay.length == 2) {
+
+        checkForMatch();
+    }
 }
 
-flipCard(0);
-flipCard(2);
-flipCard(1);
+function createBoard() {
+    for (var i = 0; i < cards.length; i++) {
+
+        var cardElement = document.createElement('img');
+
+        cardElement.setAttribute('src', 'images/back.png');
+        cardElement.setAttribute('data-id', i);
+
+        cardElement.addEventListener('click', flipCard);
+
+        document.getElementById('game-board').appendChild(cardElement);
+    }
+
+
+}
+
+createBoard();
